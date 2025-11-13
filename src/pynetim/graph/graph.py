aaa94af73af9eca82085eaff1d9835ef097f3385
zkj <2351757794@qq.com>
2025-11-13 17:28:15 +0800

    增加相关方法

--- src/pynetim/graph/graph.py
+++ src/pynetim/graph/graph.py
@@ -29,13 +29,13 @@ class IMGraph:
             edge_weight_type (str): 边权重类型，支持' CONSTANT '、' TV '、' WC '等模式
             constant_weight (float, optional): 当使用常量权重模式时的权重值
         """
-        self.graph = graph
+        self.nx_graph = graph
         self.direction = graph.is_directed()
         self.number_of_nodes = graph.number_of_nodes()
         self.number_of_edges = graph.number_of_edges()
         self.edge_weight_type = edge_weight_type
 
-        set_edge_weight(self.graph, edge_weight_type, constant_weight)
+        set_edge_weight(self.nx_graph, edge_weight_type, constant_weight)
 
     @property
     def nodes(self):
@@ -45,7 +45,7 @@ class IMGraph:
         Returns:
             NodeView: 图中所有节点的视图
         """
-        return self.graph.nodes
+        return self.nx_graph.nodes
 
     @property
     def edges(self):
@@ -55,7 +55,7 @@ class IMGraph:
         Returns:
             EdgeView: 图中所有边的视图
         """
-        return self.graph.edges
+        return self.nx_graph.edges
 
     def neighbors(self, node):
         """
@@ -67,7 +67,7 @@ class IMGraph:
         Returns:
             iterator: 指定节点的所有邻居节点迭代器
         """
-        return self.graph.neighbors(node)
+        return self.nx_graph.neighbors(node)
 
     def in_neighbors(self, node):
         """
@@ -81,7 +81,7 @@ class IMGraph:
         Returns:
             iterator: 指定节点的入邻居节点迭代器
         """
-        return self.graph.predecessors(node) if self.direction else self.graph.neighbors(node)
+        return self.nx_graph.predecessors(node) if self.direction else self.nx_graph.neighbors(node)
 
     def out_neighbors(self, node):
         """
@@ -93,7 +93,7 @@ class IMGraph:
         Returns:
             iterator: 指定节点的出邻居节点迭代器
         """
-        return self.graph.neighbors(node)
+        return self.nx_graph.neighbors(node)
 
     @property
     def in_degree(self):
@@ -103,7 +103,7 @@ class IMGraph:
         Returns:
             int: 图的入度视图
         """
-        return self.graph.in_degree if self.direction else self.graph.degree()
+        return self.nx_graph.in_degree if self.direction else self.nx_graph.degree()
 
     @property
     def out_degree(self):
@@ -113,7 +113,16 @@ class IMGraph:
         Returns:
             int: 图的出度视图
         """
-        return self.graph.out_degree if self.direction else self.graph.degree()
+        return self.nx_graph.out_degree if self.direction else self.nx_graph.degree()
+
+    def degree(self):
+        """
+        获取图的度。
+
+        Returns:
+            int: 图的度视图
+        """
+        return self.nx_graph.degree()
 
     def __str__(self):
         """
@@ -122,7 +131,7 @@ class IMGraph:
         Returns:
             str: 图对象的字符串描述
         """
-        return f'{self.graph}; edge_weight_type: {self.edge_weight_type}'
+        return f'{self.nx_graph} and edge_weight_type: {self.edge_weight_type}'
 
     def __repr__(self):
         """
