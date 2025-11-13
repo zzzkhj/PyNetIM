e696fab2ac28a333296f8376d6b789e43c36a413
zkj <2351757794@qq.com>
2025-11-13 17:27:56 +0800

    增加SI、SIR传染病模型

--- src/pynetim/__init__.py
+++ src/pynetim/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 # -----------------------------
 # 导入子模块对象，用户可以访问模块
@@ -12,7 +12,8 @@ from . import utils
 # 导入核心类到包顶层，用户可以直接访问
 # -----------------------------
 from .graph import IMGraph
-from .diffusion_model import IndependentCascadeModel, LinearThresholdModel, run_monte_carlo_diffusion
+from .diffusion_model import (IndependentCascadeModel, LinearThresholdModel, run_monte_carlo_diffusion,
+                              SusceptibleInfectedModel, SusceptibleInfectedRecoveredModel)
 
 # -----------------------------
 # __all__ 包含模块和顶层类
@@ -27,5 +28,7 @@ __all__ = [
     "IMGraph",
     "IndependentCascadeModel",
     "LinearThresholdModel",
+    "SusceptibleInfectedModel",
+    "SusceptibleInfectedRecoveredModel",
     "run_monte_carlo_diffusion"
 ]
--- src/pynetim/diffusion_model/__init__.py
+++ src/pynetim/diffusion_model/__init__.py
@@ -2,9 +2,13 @@ from .base_diffusion_model import BaseDiffusionModel
 from .independent_cascade_model import IndependentCascadeModel
 from .linear_threshold_model import LinearThresholdModel
 from .run_monte_carlo_diffusion import run_monte_carlo_diffusion
+from .susceptible_infected_model import SusceptibleInfectedModel
+from .susceptible_infected_recovered_model import SusceptibleInfectedRecoveredModel
 
 __all__ = [
     'IndependentCascadeModel',
     'LinearThresholdModel',
-    'run_monte_carlo_diffusion'
+    'run_monte_carlo_diffusion',
+    'SusceptibleInfectedModel',
+    'SusceptibleInfectedRecoveredModel'
 ]
