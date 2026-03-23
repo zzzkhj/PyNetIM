26955b6ad070ef33e2237abd5fe194710694c23a
zzzkhj <>
2026-03-23 23:16:56 +0800

    Update documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,6 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.2.2',
-    LANGUAGE: 'zh-CN',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
